1365dad46e59d1db5ae2c868d57c04a3b7b78862
Amanda Myntti <79533728+mmanteli@users.noreply.github.com>
2025-04-10 20:45:08 +0300

    Wow the lexer actually works

--- lexer.py
+++ lexer.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 
 # this dict and the list below was done using ChatGPT
@@ -54,11 +53,15 @@ token_regex = '|'.join(
 matcher = re.compile(token_regex)
 
 test_snippet = """
+function add_one acts on x,
+return +(x,1).
+Done.
+
 Let x be 5.
-Let y be (x,1,1).
+Let y be and(x,1,1).
 Let z be "hiya".
 
-For i in list,
+For i in list y,
     if i > 1,
         print i.
     Done.
@@ -66,47 +69,51 @@ For i in list,
         Modify i with function add_one.
     Done.
 Done.
+
+Let x be 1.
+While x == 1,
+Let x be +(x,1).
+Done.
 """
-
-print(ordered_token_names[37-1])
-
-def analyze_tuple(t):
-    non_empty_indices = [i for i, val in enumerate(t) if val != ""]
-    count = len(non_empty_indices)
-    index = non_empty_indices[0] if count == 1 else None
-    if count == 1:
-        value = [val for i, val in enumerate(t) if val != ""][0]
-    else:
-        value = None
-    return count, index, value
 
 line_in_code = 0
 last_index_covered = 0
-
+parenthesis_check = 0
+parenthesis_location = []
 tokens_to_forward = []
 for i in matcher.finditer(test_snippet):
+    #print(f'LINE IN CODE {line_in_code}')
     matched_segment, location, token = i.group(),i.span(), i.lastgroup
-    print(matched_segment, location, token)
+    #print(matched_segment)
 
     # check for errors
     # unrecognized segment was found:
     assert location[0] == last_index_covered, f'Syntax error on line {line_in_code}, segment "{test_snippet[last_index_covered:location[0]]}" not recognized.'
     last_index_covered = location[1]
 
+    # period or comma in the end of line:
+    if "\n" in matched_segment:
+        if tokens_to_forward != []:
+            assert tokens_to_forward[-1]["token"] in ["DOT", "COMMA"], f'Syntax error on line {line_in_code}, missing "." or ",".'
+    # parenthesis error
+    if token in ["LPAREN", "RPAREN"]:
+        if token == "LPAREN":
+            parenthesis_check += 1
+            parenthesis_location.append(line_in_code)
+        else:
+            parenthesis_check -=1
+            assert parenthesis_check>=0, f'Syntax error on line {line_in_code}, unclosed parenthesis ")".'
+            parenthesis_location.pop(-1)
+    if token in ["DOT"]:
+        assert parenthesis_check == 0, f'Syntax error on line {line_in_code}, missing parenthesis ")"'
+
     if token not in ["WHITESPACE", "COMMENT"]:
         tokens_to_forward.append(dict(token=token, value=matched_segment, span=location, line=line_in_code))
 
-    if matched_segment == "\n":
-        line_in_code +=1
-    #print(f"NOW IN INDEX {last_index_covered}")
-    #print(matched_segment, location, token)
+    if "\n" in matched_segment:
+        line_in_code += matched_segment.count("\n")
 
-    #count, index, value = analyze_tuple(i)
-    #assert count==1, "Syntax error at NOT IMPLEMENTED"
-    #print(ordered_token_names[index], value)
-
-#for k, v, r in zip(TOKENS.keys(), ordered_token_names, token_regex.split("|")):
-#    print(k, v, r)
+assert parenthesis_check == 0, f'Syntax error, unclosed parenthesis on line(s) {",".join([str(i) for i in parenthesis_location])}.'
 
 print(tokens_to_forward)
 
